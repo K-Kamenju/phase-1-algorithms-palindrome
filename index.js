8f51ad7ef521c4be4797456066b2ebf0ae23e580
Caleb <kamenjuhkiiru@gmail.com>
2023-10-06 07:43:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Write your algorithm here 
+  const originalString = word.split('');
+  const reversedString = originalString.reverse()
+  const reJoinedString = reversedString.join('')
+  console.log(reJoinedString)
+
+  //the if condition
+  if(word === reJoinedString) {
+    return true
+  } else {
+    return false
+  }
 }
+
+
+
+// isPalindrome("kamenju") // this is used to test whether the code is working till the console.log point
+
+
+
+
 
 /* 
   Add your pseudocode here
+
+function isPalindrome(word) {
+  if(word === word.reversed()) {
+    return true
+  } else {
+    return false
+  }
+}
+
+using the inbuilt js methods i.e, split, reverse and join
+
+const splitString = word.split(" ") //NOte: the split function needs to have an empty string as the arguement
+const reversedString = splitString.reverse()
+const reJoinedString = reversedString.join(" ")
+
+
+
 */
+
+
 
 /*
   Add written explanation of your solution here
+
+To check is a word is a palindorome we have to reverse the order of letter and check if they are equal
+
+This is why we use the split, reverse and join methods
+
+
 */
 
 // You can run `node index.js` to view these console logs
